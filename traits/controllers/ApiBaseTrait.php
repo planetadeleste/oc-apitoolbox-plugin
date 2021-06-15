4a05c70ca2441a0187254c2b27a07cf7a2074926
Alvaro Canepa <bfpdevel@gmail.com>
2021-06-15 16:44:49 -0300

    - Add locale param to tr method

--- traits/controllers/ApiBaseTrait.php
+++ traits/controllers/ApiBaseTrait.php
@@ -17,63 +17,143 @@ use System\Classes\PluginManager;
 trait ApiBaseTrait
 {
     /**
+     * @var \Lovata\Toolbox\Classes\Collection\ElementCollection
+     */
+    public $collection;
+    /**
+     * @var \Lovata\Toolbox\Classes\Item\ElementItem
+     */
+    public $item;
+    /**
      * @var \Lovata\Buddies\Models\User|\RainLab\User\Models\User|null
      */
     protected $user;
-
     /**
      * @var \Model
      */
     protected $obModel;
-
     /**
      * @var string
      */
     protected $modelClass;
-
     /**
      * API Resource collection class used for list items
      *
      * @var null|string
      */
     protected $listResource = null;
-
     /**
      * API Resource collection class used for index
      *
      * @var null|string
      */
     protected $indexResource = null;
-
     /**
      * API Resource class for load item
      *
      * @var null|string
      */
     protected $showResource = null;
-
     /**
      * @var bool
      */
     protected $exists = false;
 
     /**
-     * @var \Lovata\Toolbox\Classes\Collection\ElementCollection
+     * @param string      $message
+     * @param array       $options
+     * @param string|null $locale
+     *
+     * @return string
      */
-    public $collection;
+    public static function tr(string $message, array $options = [], $locale = null): string
+    {
+        if (!PluginManager::instance()->hasPlugin('RainLab.Translate')) {
+            return $message;
+        }
+
+        if (!\RainLab\Translate\Models\Message::$locale) {
+            \RainLab\Translate\Models\Message::setContext(
+                \RainLab\Translate\Classes\Translator::instance()->getLocale()
+            );
+        }
+
+        return \RainLab\Translate\Models\Message::trans($message, $options, $locale);
+    }
 
     /**
-     * @var \Lovata\Toolbox\Classes\Item\ElementItem
+     * @param \Exception $obException
+     * @param int        $iStatus
+     *
+     * @return \Illuminate\Http\JsonResponse
      */
-    public $item;
+    public static function exceptionResult($obException, $iStatus = 403)
+    {
+        trace_log($obException);
+        Result::setFalse();
+        if (!input('silently')) {
+            Result::setMessage($obException->getMessage());
+        }
+        return response()->json(Result::get(), $iStatus);
+    }
 
+    /**
+     * @return bool
+     */
+    public function exists()
+    {
+        return $this->exists;
+    }
+
+    /**
+     * @return \Lovata\Buddies\Models\User|\RainLab\User\Models\User|null
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSortColumn(): ?string
+    {
+        return $this->propertyExists('sortColumn') ? $this->sortColumn : null;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSortDirection(): ?string
+    {
+        return $this->propertyExists('sortDirection') ? $this->sortDirection : 'desc';
+    }
 
     /**
      * @return string
      */
-    public function getModelClass()
+    public function getPrimaryKey(): string
     {
-        return $this->modelClass;
+        return $this->propertyExists('primaryKey') ? $this->primaryKey : 'id';
+    }
+
+    protected function setResources()
+    {
+        if ($this->getListResource()
+            && $this->getIndexResource()
+            && $this->getShowResource()
+            || !$this->getModelClass()) {
+            return;
+        }
+
+        $classname = ltrim(static::class, '\\');
+        $arPath = explode('\\', $this->getModelClass());
+        $name = array_pop($arPath);
+        [$author, $plugin] = explode('\\', $classname);
+        $resourceClassBase = join('\\', [$author, $plugin, 'Classes', 'Resource', $name]);
+        $this->showResource = $resourceClassBase.'\\ShowResource';
+        $this->listResource = $resourceClassBase.'\\ListCollection';
+        $this->indexResource = $resourceClassBase.'\\IndexCollection';
     }
 
     /**
@@ -101,75 +181,11 @@ trait ApiBaseTrait
     }
 
     /**
-     * @return bool
-     */
-    public function exists()
-    {
-        return $this->exists;
-    }
-
-    /**
-     * @return \Lovata\Buddies\Models\User|\RainLab\User\Models\User|null
-     */
-    public function getUser()
-    {
-        return $this->user;
-    }
-
-    /**
-     * @param string $message
-     * @param array  $options
-     *
      * @return string
      */
-    public static function tr($message, $options = [])
+    public function getModelClass()
     {
-        if (!PluginManager::instance()->hasPlugin('RainLab.Translate')) {
-            return $message;
-        }
-
-        if (!\RainLab\Translate\Models\Message::$locale) {
-            \RainLab\Translate\Models\Message::setContext(
-                \RainLab\Translate\Classes\Translator::instance()->getLocale()
-            );
-        }
-
-        return \RainLab\Translate\Models\Message::trans($message, $options);
-    }
-
-    /**
-     * @param \Exception $obException
-     * @param int        $iStatus
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public static function exceptionResult($obException, $iStatus = 403)
-    {
-        trace_log($obException);
-        Result::setFalse();
-        if (!input('silently')) {
-            Result::setMessage($obException->getMessage());
-        }
-        return response()->json(Result::get(), $iStatus);
-    }
-
-    protected function setResources()
-    {
-        if ($this->getListResource()
-            && $this->getIndexResource()
-            && $this->getShowResource()
-            || !$this->getModelClass()) {
-            return;
-        }
-
-        $classname = ltrim(static::class, '\\');
-        $arPath = explode('\\', $this->getModelClass());
-        $name = array_pop($arPath);
-        [$author, $plugin] = explode('\\', $classname);
-        $resourceClassBase = join('\\', [$author, $plugin, 'Classes', 'Resource', $name]);
-        $this->showResource = $resourceClassBase.'\\ShowResource';
-        $this->listResource = $resourceClassBase.'\\ListCollection';
-        $this->indexResource = $resourceClassBase.'\\IndexCollection';
+        return $this->modelClass;
     }
 
     /**
@@ -201,29 +217,5 @@ trait ApiBaseTrait
         }
 
         return null;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getSortColumn(): ?string
-    {
-        return $this->propertyExists('sortColumn') ? $this->sortColumn : null;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getSortDirection(): ?string
-    {
-        return $this->propertyExists('sortDirection') ? $this->sortDirection : 'desc';
-    }
-
-    /**
-     * @return string
-     */
-    public function getPrimaryKey(): string
-    {
-        return $this->propertyExists('primaryKey') ? $this->primaryKey : 'id';
     }
 }
