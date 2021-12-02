d48bd1cb48c95c9eabfab282f2c8d48ff774da28
Alvaro Canepa <bfpdevel@gmail.com>
2021-12-02 05:06:41 -0300

    - New ApiResourceHandler class

---
+++ classes/event/ApiResourceHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace PlanetaDelEste\ApiToolbox\Classes\Event;
+
+use October\Rain\Events\Dispatcher;
+use PlanetaDelEste\ApiToolbox\Classes\Resource\Base;
+
+abstract class ApiResourceHandler
+{
+    public function subscribe(Dispatcher $obEvent)
+    {
+        $sEvent = app($this->getResourceClass())->event();
+        if (!$sEvent) {
+            return;
+        }
+
+        $obEvent->listen($sEvent, function ($obResourse, $arData) {
+            return $this->extendResource($obResourse, $arData);
+        });
+    }
+
+    /**
+     * @return string
+     */
+    abstract protected function getResourceClass(): string;
+
+    /**
+     * @param Base  $obResource
+     * @param array $arData
+     *
+     * @return array
+     */
+    abstract protected function extendResource(Base $obResource, array $arData): array;
+}
