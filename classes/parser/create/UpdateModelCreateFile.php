a22f5f5c23b0f4191e8420d582a316b9879a0125
Alvaro Canepa <bfpdevel@gmail.com>
2021-12-20 10:17:57 -0300

    - Fix UpdateModelCreateFile console command

--- classes/parser/create/UpdateModelCreateFile.php
+++ classes/parser/create/UpdateModelCreateFile.php
@@ -9,7 +9,7 @@ class UpdateModelCreateFile extends CommonCreateFile
     /** @var string */
     protected $sFile = 'update_{{lower_controller}}_table.php';
     /** @var string */
-    protected $sFolderPath = '/{{lower_author}}/{{lower_plugin}}/updates/{{lower_version}}';
+    protected $sFolderPath = '/{{lower_author}}/{{lower_plugin}}/updates/{{lower_version}}/';
     /** @var string */
     protected $sTemplatePath = '/planetadeleste/apitoolbox/classes/parser/templates/update_model.stub';
 
